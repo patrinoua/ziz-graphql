6043d5746a7529de58ee88e54256e83f457f1b8d
Mona Lisa <patrinoua@gmail.com>
2020-07-25 00:24:36 +0200

    add react router and firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 
 import BoatList from './components/BoatList/'
@@ -6,7 +7,10 @@ import BoatList from './components/BoatList/'
 function App() {
   return (
     <div className='App'>
-      <BoatList />
+      <BrowserRouter>
+        <Route exact path='/' render={() => <BoatList />} />
+        <Route exact path='/search' render={() => <BoatList />} />
+      </BrowserRouter>
     </div>
   )
 }
